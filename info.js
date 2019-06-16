fa0be3a92ccf6d4e9197068d72fa6730924dbb60
Nuggetmeister <47368822+icravenuggets@users.noreply.github.com>
2019-06-15 18:39:32 -0600

    Updated info.js

--- info.js
+++ info.js
@@ -1,6 +1,6 @@
 // Wizard Game
 // Alex Chen
-// April 29, 2019
+// June 17, 2019
 //
 //
 // Extra for Experts:
@@ -11,14 +11,38 @@
 // - Automatic movement by the spells in an array
 // - Cooldown on spells cast
 // - 
+// - 
 //
 //
 // See Proposal.md for "Needs to haves" and "Nice to haves"
 //
 //
+//
+//
 // See history.md for milestones
 //
-// Beta testing
+//
+//
+//
+// Beta testing comments
+//
+// David:
+// - Instructions incredibly unclear
+//
+// Aric:
+// - Menus and game look very boring with no interesting graphics
+// - Lack of sound also uninteresting
+// - Spells flashing, doesn't look smooth
+//
+// In response to beta testing:
+// - I added instructions to the pause screen, and added instructions on the main menu on how to pause
+// - Reworked and updated graphics
+// - Added many sounds, including background sound, sounds when navigating menus, spell casting, and walking
+// - The way I added everything into the grid, there is no way around the flashing unfortunately
+//
+//
+//
+//
 //
 // Reflection
 //
